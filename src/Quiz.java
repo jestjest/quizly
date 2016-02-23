410ea91c8cd39305e583a3079a2ef4f53c408db4
Jestin Ma <jestinm@stanford.edu>
2016-02-22 20:31:54 -0800

    Initial quiz class and commit.

---
+++ src/Quiz.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Quiz {
+	private ArrayList<Question> questions;
+	boolean singlePage;
+	boolean randomOrder;
+	boolean immediateFeedback;
+	String description;
+	String purpose;
+	long timeTaken;
+	int numQuestions;
+	
+}
