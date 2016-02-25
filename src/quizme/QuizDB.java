3f1bbcb847312dde30df6617d49b49381664c423
laurenblake <lblake@stanford.edu>
2016-02-25 14:29:00 -0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.classpath
    #	WebContent/answerQuestion.jsp
    #	WebContent/createQuestion.jsp
    #	WebContent/showQuestion.jsp
    #	src/quizme/Question.java
    #	src/quizme/QuizDB.java

--- src/quizme/QuizDB.java
+++ src/quizme/QuizDB.java
@@ -12,7 +12,11 @@ public class QuizDB {
 	
 	private void createQuizTable() {
 		try {
+<<<<<<< HEAD
 			PreparedStatement pstmt = db.getPreparedStatement("CREATE TABLE IF NOT EXISTS quizes ( quizid INT, name VARCHAR(128), description VARCHAR(128), purpose VARCHAR(128), numOfQuestions INT, randomOrder BOOL, "
+=======
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS quizzes (quizid INT, name CHAR(64), description CHAR(64), numOfQuestions INT, randomOrder BOOL, "
+>>>>>>> origin/master
 					+ "multiplePages BOOL, immediateCorrection BOOL)");
 			pstmt.executeUpdate();
 		} catch (SQLException e) {
