06d9e40499dbbb388cb919502939bab94cd6c769
laurenblake <lblake@stanford.edu>
2016-03-04 09:28:45 -0800

    Updated quiz class

--- src/quizme/quizzes/Quiz.java
+++ src/quizme/quizzes/Quiz.java
@@ -1,18 +1,65 @@
 // Quiz.java
-package quizme.quizzes;
+package src.quizme.quizzes;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * Quiz class encapsulating a single quiz and its information.
  */
 public class Quiz {
+	private int quizid;
+	private String name;
+	private String description;
+	private long startTime; //start time??
+	private long endTime;
+	private int numOfQuestions;
+	
+	private boolean singlePage;
+	private boolean randomOrder;
+	private boolean immediateFeedback; //from quiz summary page, not database
+	private boolean practiceMode; //from quiz summary page, not database
+	
 	private ArrayList<Question> questions;
-	boolean singlePage;
-	boolean randomOrder;
-	boolean immediateFeedback;
-	String description;
-	String purpose;
-	long timeTaken;
-	int numQuestions;
+	private ArrayList<Integer> questionIndexes;
+	
+	public Quiz(int quizid, String name, String description, int numOfQuestions, boolean singlePage, boolean randomOrder, 
+			boolean immediateFeedback, boolean practiceMode) {
+		this.quizid = quizid;
+		this.name = name;
+		this.description = description;
+		this.numOfQuestions = numOfQuestions;
+		
+		this.singlePage = singlePage;
+		this.randomOrder = randomOrder;
+		this.immediateFeedback = immediateFeedback;
+		this.practiceMode = practiceMode;
+		
+		this.questions = new ArrayList<Question>(numOfQuestions);
+		this.questionIndexes = new ArrayList<Integer>(numOfQuestions);
+		setQuestionOrder();
+	}
+	
+	private void setQuestionOrder() {
+		for (int i = 0; i < numOfQuestions; i++)
+			questionIndexes.set(i, i);
+		if (randomOrder) Collections.shuffle(questionIndexes);
+	}
+	
+	public void setQuestion(Integer order, Question question) {
+		int index = questionIndexes.get(order);
+		questions.set(index, question);
+	}
+	
+	public Question getQuestion(int index) {
+		return questions.get(index);
+	}
+	
+	public void beginTiming() {
+		startTime = System.currentTimeMillis();
+	}
+	
+	public void endTiming() {
+		endTime = System.currentTimeMillis();
+	}
 }
