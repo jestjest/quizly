172aa24af794e0e466ad468ed5146faeff69891a
Hadi Pouransari <pouransari@gmail.com>
2016-03-07 18:21:15 -0800

    Update quizResultsServlet
    Update QuizResultServlet to read ParameterMap, and create a concatenated String for each question.
    The key format is: "response_order_contentOrder".
    A helper class ParamKey is also added.

---
+++ src/quizme/ParamKey.java
@@ -0,0 +1,25 @@
+package quizme;
+
+public class ParamKey {
+	
+	public int questionOrder;
+	public int contentOrder;
+
+	public ParamKey( ) {
+	}
+
+	
+	public boolean parseKey( String key ) {
+		try {
+			String[] output = key.split("_");
+			if ( output[0] != "response" ) {
+				return false;
+			}
+			questionOrder = Integer.parseInt(output[1]);
+			contentOrder = Integer.parseInt(output[2]);
+			return true;
+		} catch ( Exception e ) {
+			return false;
+		}
+	}
+}
